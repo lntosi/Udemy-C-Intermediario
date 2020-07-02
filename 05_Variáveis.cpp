15c8efbb2e5ee953cf8d9c6b84278d36a5442990
e_lntosi <tosi@stefaninirafael.com>
2020-07-02 11:08:51 -0300

    Arquivos renomeados de acordo com a aula do Udemy

---
+++ 05_Variáveis.cpp
@@ -0,0 +1,13 @@
+// Declarando variáveis
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int idade;
+	idade = 20;
+	cout << idade << endl;
+	return 0;
+}
---
+++ 06_Operadores.cpp
@@ -0,0 +1,16 @@
+// Operadores binários - atuam sobre dois argumentos ( + - / * % )
+// Operadores uniários - atuam sobre um argumento(++ -- += -= *= /= )
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n1, n2, resultado;
+	n1 = 10;
+	n2 = 20;
+	resultado = n1 + n2;
+	cout << resultado << endl;	
+	return 0;
+}
