274eb81ab366772ca3da5f0546ba7d0edad829d5
e_lntosi <tosi@stefaninirafael.com>
2020-07-15 10:30:38 -0300

    Update de aulas 7 a 10

---
+++ 07_VariávelCaracter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	char c = 'a';
+	cout << (int)c << endl;
+	
+	return 0;
+}
---
+++ 08_Condicional1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+	int dinheiro = 0;
+
+	if(dinheiro > 10)
+	{
+		cout << "Eu vou ao cinema!" << endl;
+	}
+	else
+	{
+		cout << "Eu ficarei em casa!" << endl;
+	}
+
+	return 0;
+}
