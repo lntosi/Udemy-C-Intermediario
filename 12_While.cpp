69db03632e63f288f1f140a59af4bdeca39aea2c
lntosi <lntosi@hotmail.com>
2020-08-06 20:02:22 -0300

    Upload - 12_While.cpp, 13_For.cpp

---
+++ 12_While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int n;
+	cout << "Digite um numero: ";
+	cin >> n;
+	
+	while(n > 0)
+	{
+		cout << n << endl;
+		n--;
+	}
+	return 0;
+}
